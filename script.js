1553ad2c574937d93ab28788662fdb53602d959d
Winnie Jeng <winniewjeng@gmail.com>
2018-08-24 11:21:54 -0700

    output declaration of winner

--- script.js
+++ script.js
@@ -7,30 +7,30 @@ var gameBoard = [
 var isPlayer1 = false; //set isPlayer1 to false
 
 function clicked(row, col) {
-    if (gameBoard[row][col] == null) {
-        if (!win && !full) { // document.getElementById('box' + boxNum).style.backgroundColor = 'blue';
-            if (isPlayer1) {
-                document.getElementById('box' + (row * 3 + col + 1)).textContent = 'O';
-                isPlayer1 = !isPlayer1;
-                gameBoard[row][col] = 'O';
-                console.log(!win);
-            } else {
-                document.getElementById('box' + (row * 3 + col + 1)).textContent = 'X';
-                isPlayer1 = !isPlayer1;
-                gameBoard[row][col] = 'X';
-            }
+    console.log('asd');
+    if (gameBoard[row][col] == null && checkWin() == false) {
+        if (isPlayer1) {
+            document.getElementById('box' + (row * 3 + col + 1)).textContent = 'O';
+            isPlayer1 = !isPlayer1;
+            gameBoard[row][col] = 'O';
+            // console.log(win);
+        } else {
+            document.getElementById('box' + (row * 3 + col + 1)).textContent = 'X';
+            isPlayer1 = !isPlayer1;
+            gameBoard[row][col] = 'X';
+        }
+        if (checkWin() || checkBoardFull()) {
         }
     }
 }
 
 //check if a player has won
-var win = checkWin();
-
+// var win = checkWin();
 function checkWin() {
     //8 cases of winning:
-    //win-1: row 0
+    // win-1: row 0
     if (gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][1] != null) {
-        if (gameBoard[0][0] == 'X') {
+        if (gameBoard[0][1] == 'X') {
             console.log("Player 1 won!");
         } else {
             console.log("Player 2 won!");
@@ -39,7 +39,7 @@ function checkWin() {
     }
     //win-2: row 1
     else if (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][1] != null) {
-        if (gameBoard[1][0] == 'X') {
+        if (gameBoard[1][1] == 'X') {
             console.log("Player 1 won!");
         } else {
             console.log("Player 2 won!");
@@ -48,7 +48,7 @@ function checkWin() {
     }
     //win-3: row 2
     else if (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[2][1] != null) {
-        if (gameBoard[2][0] == 'X') {
+        if (gameBoard[2][1] == 'X') {
             console.log("Player 1 won!");
         } else {
             console.log("Player 2 won!");
@@ -56,16 +56,7 @@ function checkWin() {
         return true;
     }
     //win-4 col 0
-    else if (gameBoard[0][0] == gameBoard[1][0] && gameBoard[1][0] == gameBoard[2][0] && gameBoard[1][0] != null) {
-        if (gameBoard[0][0] == 'X') {
-            console.log("Player 1 won!");
-        } else {
-            console.log("Player 2 won!");
-        }
-        return true;
-    }
-    //win-5 col 1
-    else if (gameBoard[1][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[1][2] && gameBoard[1][1] != null) {
+    else if (gameBoard[0][0] == gameBoard[0][1] && gameBoard[0][1] == gameBoard[0][2] && gameBoard[0][1] != null) {
         if (gameBoard[0][1] == 'X') {
             console.log("Player 1 won!");
         } else {
@@ -73,9 +64,18 @@ function checkWin() {
         }
         return true;
     }
+    //win-5 col 1
+    else if (gameBoard[0][1] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][1] && gameBoard[1][1] != null) {
+        if (gameBoard[1][1] == 'X') {
+            console.log("Player 1 won!");
+        } else {
+            console.log("Player 2 won!");
+        }
+        return true;
+    }
     //win-6 col 2
-    else if (gameBoard[2][0] == gameBoard[2][1] && gameBoard[2][1] == gameBoard[2][2] && gameBoard[2][1] != null) {
-        if (gameBoard[0][2] == 'X') {
+    else if (gameBoard[0][2] == gameBoard[1][2] && gameBoard[1][2] == gameBoard[2][2] && gameBoard[1][2] != null) {
+        if (gameBoard[1][2] == 'X') {
             console.log("Player 1 won!");
         } else {
             console.log("Player 2 won!");
@@ -106,7 +106,7 @@ function checkWin() {
 }
 
 //var full = checkBoardFull;
-var full = checkBoardFull();
+// var full = checkBoardFull();
 
 function checkBoardFull() {
     for (var i = 0; i < 3; i++) {
